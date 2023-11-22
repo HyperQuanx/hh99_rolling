6d6d2d0bd545945da1d4d8d6dc6a6b22c2646eec
hdayeon <147478174+hdayeon@users.noreply.github.com>
2023-11-22 23:21:24 +0900

    Update Router.jsx
    Router.jsx 라우터 path 변경

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Main from '../pages/Main/Main';
-import MyPage from '../pages/MyPage/MyPage';
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Main from '../pages/Main/Main'
+import MyPage from '../pages/MyPage/MyPage'
+import Layout from '../pages/Layout/Layout'
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/mypage" element={<MyPage />} />
-      </Routes>
+      <Layout>
+        <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/mypage/:userId" element={<MyPage />}  />
+        </Routes>
+      </Layout>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-export default Router;
+export default Router
