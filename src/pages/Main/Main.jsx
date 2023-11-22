5fcfab8aa19108d409043b3fbeab53c593c308a0
한덕용 <128759865+HyperQuanx@users.noreply.github.com>
2023-11-22 22:45:33 +0900

    Update Main.jsx
    빈값

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,14 +1,1 @@
-import React from 'react';
-import Navbar from '../Layout/Navbar';
-import Home from '../Home/Home';
 
-const Main = () => {
-  return (
-    <div style={{ backgroundColor: '#E2E2E2' }}>
-      <Navbar />
-      <Home />
-    </div>
-  );
-};
-
-export default Main;
