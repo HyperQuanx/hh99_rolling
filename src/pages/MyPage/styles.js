d9a5d345ac7c1d370c189e6a3f2daaff34365c04
hdayeon <147478174+hdayeon@users.noreply.github.com>
2023-11-23 17:14:23 +0900

    Update styles.js
    MyPage컴포넌트 styles

--- src/pages/MyPage/styles.js
+++ src/pages/MyPage/styles.js
@@ -1,14 +1,13 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const StMyWrap = styled.div`
   width: 1280px;
-  height: 100%;
+  height: 70vh;
   display: flex;
-
-`;
+`
 
 const StName = styled.div`
   font-size: 30px;
-`;
+`
 
-export { StMyWrap, StName };
+export {StMyWrap,StName}
