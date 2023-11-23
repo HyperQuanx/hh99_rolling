56a7c0a7da122963918c24000f6036a35a63b74c
한덕용 <128759865+HyperQuanx@users.noreply.github.com>
2023-11-23 13:14:05 +0900

    Update styles.js

--- src/pages/MyPage/MyPapers/styles.js
+++ src/pages/MyPage/MyPapers/styles.js
@@ -1,11 +1,12 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const StPaperWrap = styled.div`
   width: 80%;
   background-color: #eeeeee;
   border: 3px solid #111111;
   border-left: none;
-  `
+  border-radius: 0 10px 10px 0;
+`;
 
 const StInput = styled.input`
   width: 70%;
@@ -14,22 +15,25 @@ const StInput = styled.input`
   background-color: #ffffff;
   border: none;
   border-radius: 10px;
-`
+`;
 
 const StPaperBox = styled.div`
+  width: 96%;
+  height: 80%;
   margin: 20px;
-  height: 100%;
-  background-color: #bbbbbb;
+  background-image: url('../public/wkf.png');
+  background-size: cover;
   display: flex;
   justify-content: space-around;
   flex-wrap: wrap;
-`
+`;
 
 const StPaper = styled.div`
-  width: 16%;
-  height: 200px;
+  width: 200px;
+  height: 100px;
   padding: 20px;
-  background-color: #ffffff;
-`
+  font-size: 30px;
+  background-color: rgba(255, 255, 255, 0.5);
+`;
 
-export {StPaperWrap,StInput,StPaper,StPaperBox}
+export { StPaperWrap, StInput, StPaper, StPaperBox };
