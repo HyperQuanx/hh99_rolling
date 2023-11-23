1f098983361321c56ae9e4f95c2ea58f2eaa5e14
한덕용 <128759865+HyperQuanx@users.noreply.github.com>
2023-11-23 13:14:15 +0900

    Update MyPage.jsx

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -9,19 +9,22 @@ import { StMyWrap, StName } from './styles';
 
 const MyPage = () => {
   const params = useParams();
-  const [rollingPaper, setRollingPaper] = useState(
-    {userId: '', userName: '', bio: '', comments: [{comment: ''},{comment: ''}]}
-  );
+  const [rollingPaper, setRollingPaper] = useState({
+    userId: '',
+    userName: '',
+    bio: '',
+    comments: [{ comment: '' }, { comment: '' }],
+  });
 
-  const {isError, data} = useQuery("myPage", getPapers, {
-    onSuccess: (data) => {
-      const foundData = data.find((item) => item.userId == params.userId);
+  const { isError, data } = useQuery('myPage', getPapers, {
+    onSuccess: data => {
+      const foundData = data.find(item => item.userId == params.userId);
       if (foundData) {
         setRollingPaper(foundData); // 찾은 데이터를 state에 업데이트->렌더링
-      }; 
-    }
+      }
+    },
   });
-  if (isError) return <div>마이페이지 에러 오류</div>
+  if (isError) return <div>마이페이지 에러 오류</div>;
 
   // console.log("data",data);
   // console.log("params",params);
@@ -31,11 +34,11 @@ const MyPage = () => {
     <StWrap>
       <StName>{rollingPaper.userName}님의 롤링페이지✨</StName>
       <StMyWrap>
-      <MyBio rollingPaper={rollingPaper}/>
-      <MyPapers/>
+        <MyBio rollingPaper={rollingPaper} />
+        <MyPapers />
       </StMyWrap>
     </StWrap>
-    );
+  );
 };
 
 export default MyPage;
